939fee8c4e452c1661db28f0ed26341ac7d02d78
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-08-29 00:09:11 +0200

    Create Footer.jsx

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+/**
+ * @function Footer
+ * @description A functional component that displays the footer of the page.
+ */
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
