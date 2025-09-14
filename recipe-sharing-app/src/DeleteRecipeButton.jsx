60bf04d5db8004db6dcc472d6ef6fc5afb7ce136
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-09-14 21:23:05 +0200

    Create DeleteRecipeButton.jsx

---
+++ recipe-sharing-app/src/DeleteRecipeButton.jsx
@@ -0,0 +1,10 @@
+// src/components/DeleteRecipeButton.jsx
+import useRecipeStore from '../recipeStore';
+
+const DeleteRecipeButton = ({ recipeId }) => {
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+
+  return <button onClick={() => deleteRecipe(recipeId)}>Delete</button>;
+};
+
+export default DeleteRecipeButton;
