9ae00626125b341ccd284d51226de7907124014e
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-09-17 23:04:19 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
+// Function must be named fetchUserData
 export async function fetchUserData(username) {
-  const url = `https://api.github.com/users/${username}`
-  const response = await axios.get(url)
-  return response.data
+  const url = `https://api.github.com/users/${username}`;
+  const response = await axios.get(url);
+  return response.data;
 }
