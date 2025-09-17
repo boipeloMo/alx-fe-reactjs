aeb847905bd06444add84c302237c34c4af4be3a
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-09-17 22:56:52 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -7,8 +7,7 @@ export const fetchUserData = async (username) => {
     const response = await axios.get(`${GITHUB_API_URL}/users/${username}`);
     return response.data;
   } catch (error) {
-    // This is the key part to check.
-    // Ensure you throw an error to be caught by the calling function.
+  
     throw new Error('User not found or an API error occurred.');
   }
 };
