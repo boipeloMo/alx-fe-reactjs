3ce69277bb9a4d33dab04a82623f6ce7a5d6d701
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-12 18:24:50 +0200

    Create App.jsx

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PostsComponent from "./components/PostsComponent.jsx";
+
+function App() {
+  return (
+    <div>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
