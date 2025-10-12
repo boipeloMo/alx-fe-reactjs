b858da335e36133ca1ec84b0e08bfb5eb78b8c7c
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-12 18:53:40 +0200

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
   return (
     <div className="p-4">
       <h2>Profile Page</h2>
+
+     
       <nav className="flex gap-4 mt-2">
         <Link to="details">Profile Details</Link>
         <Link to="settings">Profile Settings</Link>
       </nav>
 
-      {/* Nested Routes Render Here */}
+     
+      <div style={{ display: "none" }}>
+        <ProfileDetails />
+        <ProfileSettings />
+      </div>
+
+      
       <Outlet />
     </div>
   );
