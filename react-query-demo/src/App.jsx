cf3934359f028fc12d6747c457d08a3c2a2ea042
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-12 18:32:21 +0200

    Update App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import PostsComponent from "./components/PostsComponent.jsx";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <PostsComponent />
-    </div>
+  
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1 className="text-2xl font-bold text-center mt-6">
+          Advanced Data Handling with React Query
+        </h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
