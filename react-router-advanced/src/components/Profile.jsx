a001f966167d6d52d013d1decf7abbd7dcd6bb22
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-12 18:54:57 +0200

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 import ProfileDetails from "./ProfileDetails";
 import ProfileSettings from "./ProfileSettings";
 
@@ -8,22 +8,14 @@ const Profile = () => {
     <div className="p-4">
       <h2>Profile Page</h2>
 
-     
+      {/* Navigation Links */}
       <nav className="flex gap-4 mt-2">
         <Link to="details">Profile Details</Link>
         <Link to="settings">Profile Settings</Link>
       </nav>
 
-     
-      <div style={{ display: "none" }}>
-        <ProfileDetails />
-        <ProfileSettings />
-      </div>
-
-      
-      <Outlet />
-    </div>
-  );
-};
-
-export default Profile;
+      {/* ✅ Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
