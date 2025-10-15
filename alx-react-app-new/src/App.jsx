f237db1c9b1095854f8b4648f72631574572941a
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-15 23:38:22 +0200

    Replace layout components with Counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <h1 style={{ textAlign: "center" }}>Simple Counter App</h1>
+      <Counter />
     </div>
   );
 }
