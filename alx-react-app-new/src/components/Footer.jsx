ccc8769115e775ab824429328828d03ed9f9af27
boipeloMo <132396956+boipeloMo@users.noreply.github.com>
2025-10-15 23:31:13 +0200

    Add Footer component with styling and copyright text

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+        padding: "15px",
+        marginTop: "20px",
+      }}
+    >
+      <p>&copy; 2025 My Favorite Cities App. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
